74d9f438c7fcef1375ede8818432c307e1175afa
Alexandre J. Raymond <alexandre.j.raymond@gmail.com>
2023-03-05 21:14:40 -0500

    Add getopt support

--- arve.c
+++ arve.c
@@ -1,7 +1,33 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void usage(const char *argv)
+{
+    printf("Usage: %s <program.bin>\n", argv);
+    printf("    -h: Print this message\n");
+}
 
 int main(int argc, char* argv[])
 {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "h")) != -1) {
+        switch (opt) {
+            case 'h':
+            default:
+                usage(argv[0]);
+                exit(1);
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "ERROR: Missing program name\n");
+        exit(1);
+    }
+
     return 0;
 }
 
